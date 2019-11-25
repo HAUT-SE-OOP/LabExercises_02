116613511c7e4457a2ec5a5ccae4da3157ff4060
201821010211 <56548775+201821010211@users.noreply.github.com>
2019-11-25 19:52:09 +0800

    Create HugeInt.cpp
    This is my LabExercises_02
    My student id:201821010211

---
+++ 201821010211/Lab2/HugeInt.cpp
@@ -0,0 +1,35 @@
+// Lab 2: Hugeint.cpp
+// HugeInt member-function and friend-function definitions.
+#include <iostream>
+#include <cctype> // isdigit function prototype
+#include <cstring> // strlen function prototype
+using namespace std;
+
+#include "Hugeint.h" // HugeInt class definition
+
+// default constructor; conversion constructor that converts
+// a long integer into a HugeInt object
+HugeInt::HugeInt( long value )
+{
+   // initialize array to zero
+   for ( int i = 0; i <= 29; i++ )
+      integer[ i ] = 0;
+
+   // place digits of argument into array
+   for ( int j = 29; value != 0 && j >= 0; j-- )
+   {
+      integer[ j ] = value % 10;
+      value /= 10;
+   } // end for
+} // end HugeInt default/conversion constructor
+
+// conversion constructor that converts a character string
+// representing a large integer into a HugeInt object
+HugeInt::HugeInt( const char *string )
+{
+   // initialize array to zero
+   for ( int i = 0; i <= 29; i++ )
+      integer[ i ] = 0;
+
+   // place digits of argument into array
+   int length = strlen( string );
