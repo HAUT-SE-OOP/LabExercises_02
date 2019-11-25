0a36382946e17234103f87c2dceefd4a3b4f61e3
logjj <53385110+logjj@users.noreply.github.com>
2019-11-25 20:37:16 +0800

    Add files via upload
    201816040305

---
+++ 201816040305/Lab3/RationalNumber.cpp
@@ -0,0 +1,175 @@
+// Lab 3: RationalNumber.cpp
+// RationalNumber member-function definitions.
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+#include "RationalNumber.h"
+
+// RationalNumber constructor sets n and d and calls reduction
+
+RationalNumber::RationalNumber(int n , int d)
+{
+    if(d == 0 )
+    {
+        numerator = n ;
+        denominator = 1;
+    }
+    else
+    {
+        numerator = n ;
+        denominator = d;
+    }
+}
+
+// overloaded + operator
+RationalNumber RationalNumber::operator+(  RationalNumber num )
+{
+    RationalNumber temp; // temporary result
+    if (denominator == num.denominator)
+        num.numerator = numerator+num.numerator;
+    /*else if(denominator != num.denominator)
+        {
+            num.denominator = denominator*num.denominator ;
+            num.numerator = num.numerator*denominator;
+            num.denominator = num.denominator * numerator ;
+        }*/
+        return num ;
+} // end function operator+
+
+// overloaded - operator
+RationalNumber RationalNumber::operator-(  RationalNumber num )
+{
+    RationalNumber temp; // temporary result
+    if (denominator == num.denominator)
+        {
+            num.numerator = numerator-num.numerator ;
+            num.denominator = denominator ;
+        }
+    /*else if(denominator != num.denominator)
+        {
+            num.denominator = denominator*num.denominator ;
+            num.numerator = num.numerator*;
+        }*/
+        return num ;
+}
+
+// overloaded * operator
+RationalNumber RationalNumber::operator*(  RationalNumber num )
+{
+            num.numerator = numerator*num.numerator ;
+            num.denominator = num.denominator*denominator ;
+        return num ;
+}
+
+// overloaded / operator
+RationalNumber RationalNumber::operator/(  RationalNumber num )
+{
+    if(num.numerator == 0)
+    {
+        cout << "error divede by 0"<<"regive it to 1"<<endl;
+        num.denominator = 1;
+        num.numerator = 1 ;
+    }
+    else{
+            num.numerator = denominator*num.numerator ;
+            num.denominator = num.denominator*numerator;
+    }
+        return num ;
+}
+
+
+// overloaded > operator
+bool RationalNumber::operator>(  const RationalNumber &num )
+{
+    if(numerator>num.numerator)
+        return true ;
+    else
+        return false ;
+}
+
+// overloaded < operator
+bool RationalNumber::operator<(  const RationalNumber &num )
+{
+    if(numerator<num.numerator)
+        return true ;
+    else
+        return false ;
+}
+
+
+// overloaded >= operator
+bool RationalNumber::operator>=(  const RationalNumber &num )
+{
+    if(numerator<num.numerator)
+        return false ;
+    else
+        return true ;
+}
+// overloaded <= operator
+bool RationalNumber::operator<=(  const RationalNumber &num )
+{
+    if(numerator>num.numerator)
+        return false ;
+    else
+        return true ;
+}
+
+// overloaded == operator
+bool RationalNumber::operator==(  const RationalNumber &num )
+{
+    if(num.numerator==numerator)
+        return true ;
+    else
+        return false ;
+}
+
+// overloaded != operator
+bool RationalNumber::operator!=(  const RationalNumber &num )
+{
+    if(num.numerator!=numerator)
+        return true ;
+    else
+        return false ;
+}
+// function printRational definition
+void RationalNumber::printRational() const
+{
+   if ( numerator == 0 ) // print fraction as zero
+      cout << numerator;
+   else if ( denominator == 1 ) // print fraction as integer
+      cout << numerator;
+   else
+      cout << numerator << '/' << denominator;
+} // end function printRational
+
+// function reduction definition
+void RationalNumber::reduction()
+{
+   int largest, gcd = 1;  // greatest common divisor;
+
+   largest = ( numerator > denominator ) ? numerator: denominator;
+
+   for ( int loop = 2; loop <= largest; loop++ )
+       if ( numerator % loop == 0 && denominator % loop == 0 )
+          gcd = loop;
+
+   numerator /= gcd;
+   denominator /= gcd;
+} // end function reduction
+
+
+/**************************************************************************
+ * (C) Copyright 1992-2012 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ **************************************************************************/
