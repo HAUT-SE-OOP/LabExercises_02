c237ef300ecb733615fdf043cb396925e4134095
kennnna <56779636+201816040329@users.noreply.github.com>
2019-11-25 19:01:39 +0800

    Create RationalNumber.cpp

---
+++ 201816040329/Lab3/RationalNumber.cpp
@@ -0,0 +1,219 @@
+// Lab 3: RationalNumber.cpp
+// RationalNumber member-function definitions.
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+#include "RationalNumber.h"
+
+// RationalNumber constructor sets n and d and calls reduction
+/* Implement the RationalNumber constructor. Validate d first to ensure that
+   it is a positive number and set it to 1 if not. Call the reduction utility
+   function at the end */
+   RationalNumber::RationalNumber(int k,int n)
+   {
+       if(n<=0)
+        denominator=1;
+       else
+        denominator=n;
+       numerator=k;
+       reduction();
+   }
+
+// overloaded + operator
+ RationalNumber RationalNumber::operator+(const RationalNumber &k)
+ {
+     RationalNumber p;
+     if(denominator==k.denominator)
+         {
+          p.denominator=k.denominator;
+          p.numerator=numerator+k.numerator;}
+     if(denominator!=k.denominator)
+        {
+          p.denominator=denominator*k.denominator;
+          p.numerator=numerator*k.denominator+k.numerator*denominator;}
+     return p;
+ }
+
+// overloaded - operator
+/* Write definition for overloaded operator - */
+RationalNumber RationalNumber::operator-(const RationalNumber &k)
+{
+    RationalNumber p;
+    if(denominator=k.denominator)
+    {
+        p.denominator=k.denominator;
+        p.numerator=numerator-k.numerator;
+    }
+    if(denominator!=k.denominator)
+    {
+        p.denominator=denominator*k.denominator;
+        p.numerator=numerator*k.denominator-k.numerator*denominator;
+    }
+    return p;
+}
+
+// overloaded * operator
+/* Write definition for overloaded operator * */
+RationalNumber RationalNumber::operator*(const RationalNumber &k)
+{
+    RationalNumber p;
+    p.denominator=denominator*k.denominator;
+    p.numerator=numerator*k.numerator;
+}
+// overloaded / operator
+/* Write definition for overloaded operator /. Check if the client is
+   attempting to divide by zero and report an error message if so */
+RationalNumber RationalNumber::operator/(const RationalNumber &k)
+{
+    RationalNumber p;
+    p.denominator=denominator*k.numerator;
+    p.numerator=numerator*k.denominator;
+}
+// overloaded > operator
+/* Write definition for operator > */
+bool RationalNumber::operator>(const RationalNumber &k)
+{
+    if(denominator==k.denominator)
+    {
+        if(numerator>k.denominator)
+            return true;
+    }
+    else if(numerator*k.denominator>k.numerator*denominator)
+    {
+      return true;
+
+    }
+    else
+        return false;
+}
+
+
+// overloaded < operator
+/* Write definition for operator < */
+bool RationalNumber::operator<(const RationalNumber &k)
+{
+    if(denominator==k.denominator)
+    {
+        if(numerator<k.numerator)
+            return true;
+    }
+    else if(numerator*k.denominator<k.numerator*denominator)
+    {
+        return true;
+    }
+    else
+        return false;
+}
+
+// overloaded >= operator
+/* Write definition for operator >= */
+bool RationalNumber::operator>=(const RationalNumber &k)
+{
+    if(denominator==k.denominator)
+    {
+        if(numerator>k.denominator&&numerator==k.numerator)
+            return true;
+    }
+    else if(numerator*k.denominator>k.numerator*denominator&&numerator*k.denominator==k.numerator*denominator)
+    {
+      return true;
+
+    }
+    else
+        return false;
+}
+
+// overloaded <= operator
+/* Write definition for operator <= */
+bool RationalNumber::operator<=(const RationalNumber &k)
+{
+    if(denominator==k.denominator)
+    {
+        if(numerator<k.denominator&&numerator==k.numerator)
+            return true;
+    }
+    else if(numerator*k.denominator<k.numerator*denominator&&numerator*k.denominator==k.numerator*denominator)
+    {
+      return true;
+
+    }
+    else
+        return false;
+}
+
+
+// overloaded == operator
+/* Write definition for operator == */
+bool RationalNumber::operator==(const RationalNumber &k)
+{
+    if(denominator==k.denominator&&numerator==k.numerator)
+        return true;
+    else if(denominator!=k.denominator)
+    {
+        if(numerator*k.denominator==k.numerator*denominator)
+        return true;
+
+    }
+    else
+        return false;
+}
+
+// overloaded != operator
+/* Write definition for operator != */
+bool RationalNumber::operator!=(const RationalNumber &k)
+{
+    if(denominator==k.denominator&&numerator!=k.numerator)
+    {
+        return true;
+    }
+    else if(numerator*k.denominator!=k.numerator*denominator)
+    {
+        return true;
+    }
+    else
+        return false;
+
+}
+
+// function printRational definition
+void RationalNumber::printRational() const
+{
+   if ( numerator == 0 ) // print fraction as zero
+      cout << numerator;
+   else if ( denominator == 1 ) // print fraction as integer
+      cout << numerator;
+   else
+      cout << numerator << '/' << denominator;
+} // end function printRational
+
+// function reduction definition
+void RationalNumber::reduction()
+{
+   int largest, gcd = 1;  // greatest common divisor;
+
+   largest = ( numerator > denominator ) ? numerator: denominator;
+
+   for ( int loop = 2; loop <= largest; loop++ )
+       if ( numerator % loop == 0 && denominator % loop == 0 )
+          gcd = loop;
+
+   numerator /= gcd;
+   denominator /= gcd;
+} // end function reduction
+
+
+/**************************************************************************
+ * (C) Copyright 1992-2012 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ **************************************************************************/
