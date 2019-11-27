e486e49dd592b34380cb00d5349a6b392d73172d
201826030127杨慧姗 <56498911+201826030127@users.noreply.github.com>
2019-11-27 20:13:48 +0800

    Create RationalNumber.h
    This is my Lab3.
    Student ID:201826030127

---
+++ 201826030127/Lab3/RationalNumber.h
@@ -0,0 +1,34 @@
+// Lab 3: RationalNumber.h
+// RationalNumber class definition.
+#ifndef RATIONAL_NUMBER_H
+#define RATIONAL_NUMBER_H
+
+class RationalNumber
+{
+public:
+   RationalNumber( int = 0, int = 1 ); // default constructor
+   RationalNumber operator+(const RationalNumber &);
+   RationalNumber  operator - (const RationalNumber &);
+   RationalNumber  operator * (const RationalNumber &);
+   RationalNumber  operator / (const RationalNumber &);
+
+   // relational operators
+
+   bool  operator > (const RationalNumber &);
+   bool  operator < (const RationalNumber &);
+   bool  operator >= (const RationalNumber &);
+   bool  operator <= (const RationalNumber &);
+
+   // equality operators
+   bool  operator == (const RationalNumber &);
+   bool  operator != (const RationalNumber &);
+
+   void printRational() const; // display rational number
+private:
+   int numerator; // private variable numerator
+   int denominator; // private variable denominator
+   void reduction(); // function for fraction reduction
+}; // end class RationalNumber
+
+#endif
+#endif // RATIONAL_NUMBERS_H_INCLUDED
